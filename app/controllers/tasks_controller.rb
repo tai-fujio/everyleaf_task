655cc0ad98ebb8334fb31f17ed348b21d157a965
taisaburo_fujio <taisaburo.fujio@gmail.com>
2019-10-22 15:07:58 +0900

    tasksコントローラ実装

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,27 @@
+class TasksController < ApplicationController
+  before_acition :set_task, only[:show,:edit]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+    @task = Task.new(task_params)
+  end
+
+  private
+  def set_task
+  @task = Task.find(params[:id])
+  end
+
+  def task_params
+  params.require(:task).permit(:task_name,:task_details)
+  end
+
+end
