a8e358d0e5a5a504cf0e5ac7b4b0e3a041ec6340
taisaburo_fujio <taisaburo.fujio@gmail.com>
2019-10-30 15:27:56 +0900

    終了期限、ステータステストSystemSpecに追記

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
   factory :task_test, class: Task do
     name { "デフォルト名" }
     detail { "デフォルトコンテント" }
-    deadline { Time.now + 1 }
+    deadline {"2100-01-01".to_date }
     priority { {"1": "高"}}
     status {"着手中"}
   end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,21 +1,33 @@
 require 'rails_helper'
 require 'selenium-webdriver'
+require "active_support/time"
 
 RSpec.describe Task, type: :system do
   describe "Taskテスト" do
     before do
+      @task_test0 = FactoryBot.create(:task_test,deadline: "2099-12-31".to_date)
       @task_test1 = FactoryBot.create(:task_test,name:"タスク名テスト1",detail:"タスク詳細テスト1")
       @task_test2 = FactoryBot.create(:task_test,name:"タスク名テスト2",detail:"タスク詳細テスト2")
       @task_test3 = FactoryBot.create(:task_test,name:"タスク名テスト3",detail:"タスク詳細テスト3")
     end  
     
-    context "タスク一覧のテスト" do
+    context "tasks#indexのテスト" do
       it "タスク一覧にタスク名が表示される" do
         visit tasks_path
         expect(page).to have_content "タスク名テスト1"
         expect(page).to have_content "タスク詳細テスト1"
         expect(page).to_not have_content "タスク詳細テスト4"
       end
+      it "タスク一覧に終了期限が表示される" do
+        visit tasks_path
+        expect(page).to have_content "2099-12-31"
+        expect(page).to have_content "2100-01-01"
+        expect(page).to_not have_content "2100-01-02"
+      end
+      it "タスク一覧にステータスが表示される" do
+        visit tasks_path
+        expect(page.find(".index_status",match: :first)).to have_content "着手中"
+      end  
     end
 
     context "tasks#createのテスト" ,driver: :webkit do
@@ -23,15 +35,18 @@ RSpec.describe Task, type: :system do
         visit new_task_path
         fill_in "task_name", with: "タスク名テスト"
         fill_in "task_detail", with: "タスク詳細テスト"
-        fill_in "task_deadline", with: Time.now + 1
+        fill_in "task_deadline", with: "2050-01-01".to_date
         select "高", from: "task_priority"
-        select "着手", from:  "task_status"
+        select "未着手", from:  "task_status"
         click_button("新規作成")
         expect(page.find(".show_name")).to have_content "タスク名テスト"
         expect(page.find(".show_detail")).to have_content "タスク詳細テスト"
         expect(page.find(".show_name")).to_not have_content "タスク詳細テスト"
+        expect(page.find(".show_deadline")).to have_content "2050-01-01"
+        expect(page.find(".show_deadline")).to_not have_content "2050-01-02"
         expect(page.find(".show_detail")).to_not have_content "タスク名テスト"
-        save_and_open_page
+        expect(page.find(".show_status")).to have_content "未着手"
+        expect(page.find(".show_status")).not_to have_content "完了"
       end
     end  
 
@@ -42,6 +57,10 @@ RSpec.describe Task, type: :system do
         within_window(switch_to_window(windows.last)) do 
           expect(page).to have_content "タスク名テスト3"
           expect(page).to have_content "タスク詳細テスト3"
+          expect(page).to have_content "2100-01-01"
+          expect(page).to have_content "2100-01-01"
+          expect(page).to have_content "着手中"
+          expect(page).not_to have_content "完了"
         end
       end
     end
@@ -53,7 +72,7 @@ RSpec.describe Task, type: :system do
         expect( tasks[0].native.text ).to have_content "タスク名テスト3" 
         expect(  tasks[1].native.text ).to have_content "タスク名テスト2" 
         expect(  tasks[2].native.text ).to have_content "タスク名テスト1"
-      end   
+      end
     end
   end
 end
