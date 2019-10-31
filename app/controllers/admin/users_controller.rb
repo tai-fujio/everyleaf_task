5199803cfd2a4b178f8feff00372731f655a048f
taisaburo_fujio <taisaburo.fujio@gmail.com>
2019-10-31 15:28:59 +0900

    ユーザー管理コントローラ作成

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,13 @@
+class Admin::UsersController < ApplicationController
+  def indes
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'users/indes'
+    get 'users/new'
+    get 'users/show'
+    get 'users/edit'
+  end
   root "tasks#index"
   resources :tasks
 end
