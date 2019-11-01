ec3fa7cba94db3e1ae23757aa148249ed6514142
taisaburo_fujio <taisaburo.fujio@gmail.com>
2019-11-01 15:43:52 +0900

    タスクモデルのnameカラムにindex追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_31_061017) do
+ActiveRecord::Schema.define(version: 2019_11_01_063803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_10_31_061017) do
     t.string "priority", null: false
     t.string "status", null: false
     t.bigint "user_id"
+    t.index ["name"], name: "index_tasks_on_name"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
