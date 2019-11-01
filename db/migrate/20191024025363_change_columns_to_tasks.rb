4a5ba1d5ce2233c84d0076683323b0f95496b7ed
taisaburo_fujio <taisaburo.fujio@gmail.com>
2019-11-01 18:00:17 +0900

    マイグレーション、スキーマ再作成

--- db/migrate/20191024025363_change_columns_to_tasks.rb
+++
@@ -1,9 +0,0 @@
-class ChangeColumnsToTasks < ActiveRecord::Migration[5.2]
-  def up
-    change_column_null :tasks, :detail, null:false
-  end
-
-  def down
-    change_column_null :tasks, :detail, null:true
-  end
-end
