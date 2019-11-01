ec641e14017d28e775385389c13430f55580918e
taisaburo_fujio <taisaburo.fujio@gmail.com>
2019-11-01 16:33:06 +0900

    ログイン機能バグ修正　--amend

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,6 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    # user = User.find_by(email: session_params[:email])
-    # if user&.authenticate(session_params[:password])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to tasks_path
