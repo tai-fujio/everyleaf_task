6e253fd7a9753df366a4decb4183197a432c2b77
taisaburo_fujio <taisaburo.fujio@gmail.com>
2019-11-01 16:31:45 +0900

    ログイン機能修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :log_in?
-  before_action :login_must
+  include SessionsHelper
+  # before_action :login_must
 
   private
   def login_must
+    binding.pry
     unless current_user
       redirect_to root_path
     end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,14 @@
 class SessionsController < ApplicationController
-  skip_before_action :login_must
+  # skip_before_action :login_must
   def new; end
 
   def create
-    user = User.find_by(params[:session][:email].downcase)
+    user = User.find_by(email: params[:session][:email].downcase)
     # user = User.find_by(email: session_params[:email])
     # if user&.authenticate(session_params[:password])
-    binding.pry
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      redirect_to users_path
+      redirect_to tasks_path
     else
       flash[:notice] = "ログインに失敗しました"
       render :new
