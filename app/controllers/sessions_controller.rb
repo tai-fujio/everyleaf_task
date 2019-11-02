69024f0a9923b59d82f9ab13b35bc79f566a3475
taisaburo_fujio <taisaburo.fujio@gmail.com>
2019-11-02 18:30:49 +0900

    ログイン機能バグ修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,7 @@
 class SessionsController < ApplicationController
   def new; end
   def create
-    user = User.find_by(params[:session][:email].downcase)
-    # user = User.find_by(email: session_params[:email])
-    # if user&.authenticate(session_params[:password]) 
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to tasks_path
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper
   def current_user
-    @current_user ||= User.find_by(sessions_params[:user_id])
+    @current_user ||= User.find_by(:id params[:user_id])
   end
   def log_in?
     current_user.present?
