dd7489a3413a63769411ded701e16015cb4c5cc9
taisaburo_fujio <taisaburo.fujio@gmail.com>
2019-11-03 03:15:20 +0900

    ユーザー登録画面実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,7 +6,9 @@ class Admin::UsersController < ApplicationController
     @users = User.all.paginate(page: params[:page], per_page: 10)
     @user = User.find(current_user.id)
   end
-  def new; end
+  def new
+    @user = User.new
+  end
   def show; end
   def destroy
     @user = User.find(params[:id])
@@ -21,6 +23,19 @@ class Admin::UsersController < ApplicationController
     end
   end
   def update; end
-  def create; end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "新規登録しました"
+      redirect_to admin_users_path
+    else
+      flash.now[:notice] = "登録に失敗しました"
+      render :new
+    end
+  end
   def edit; end
+  private
+  def user_params
+    params.require(:user).permit(:password_digest,:name,:email,:admin?)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
   before_validation { email.downcase! }
   has_many :tasks, dependent: :destroy
-  has_secure_password
   validates :name, presence: true, length: { maximum:30 }
-  validates :password, presence: true, length: { maximum:30 ,minimum:6 }
+  validates :password_digest, presence: true, length: { maximum:30 ,minimum:6 }
   validates :email, presence: true, uniqueness: true, length: { maximum:150 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
