3f44e037d4b9f887fd38916401d3bfc741763b76
taisaburo_fujio <taisaburo.fujio@gmail.com>
2019-11-02 17:07:07 +0900

    ログイン画面実装、sessions#create、yml追記

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
-  def new
+  def new; end
+  def create
+    user = User.find_by(params[:session][:email].downcase)
+    # user = User.find_by(email: session_params[:email])
+    # if user&.authenticate(session_params[:password]) 
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to tasks_path
+    else
+      flash[:notice] = "ログインに失敗しました"
+      render :new
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  root to: "sessions#new"
   namespace :admin do
     resources :users
   end  
-  root to: "tasks#index"
   resources :tasks
 end
