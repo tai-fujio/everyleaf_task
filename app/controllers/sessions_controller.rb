cf540fd328f37866a83b4f98da873094b6cbaf21
taisaburo_fujio <taisaburo.fujio@gmail.com>
2019-11-02 18:25:50 +0900

    ヘルパーにログイン情報取得メソッド記述

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,4 +12,9 @@ class SessionsController < ApplicationController
       render :new
     end
   end
+  def destroy
+    reset_sesson
+    flash[:notice] = "ログアウトしました"
+    redirect_to root_path
+  end
 end
