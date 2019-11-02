7d6ae45f7235304b47fffd2c41996c464ca93051
taisaburo_fujio <taisaburo.fujio@gmail.com>
2019-11-02 16:42:43 +0900

    タスク一覧画面にユーザー名表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   before_validation { email.downcase! }
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
   has_secure_password
   validates :name, presence: true, length: { maximum:30 }
   validates :password, presence: true, length: { maximum:30 ,minimum:6 }
